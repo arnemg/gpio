f0c55664e1d609af7a1d66fbae5f12d6cf7fc50b
Arne Moe-Gumø <arnemg@gmail.com>
2019-05-14 09:04:40 +0100

    Har endret på strukturen i mail construct

--- EmailModule.js
+++ EmailModule.js
@@ -5,7 +5,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'fartoysjefgumo@gmail.com',
-      pass: 'Jalla900'
+      pass: 'etellerannet'
     }
  });
 
